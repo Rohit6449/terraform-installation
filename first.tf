975308e43fb4731f135bffa4f312c79cd96b573b
Rohit6449 <rohitkr6449@gmail.com>
2025-09-29 17:45:39 +0530

    Create first.tf

---
+++ first.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-2"
+    access_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    secret_key = "xxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+resource "aws_instance" "rohitec2" {
+  ami           = "ami-0ca4d5db4872d0c28"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ec2-test"
+  }
+}
